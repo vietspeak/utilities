cd58cdbaf74baaaa557690c3e99a4dd8e49b59e5
Vuong Nguyen <yeungon@gmail.com>
2022-03-27 23:50:56 +0700

    Update utilities

--- utilities.js
+++ utilities.js
@@ -1,7 +1,6 @@
 exports.currentTimeStamp = () => Math.floor(new Date().getTime() / 1000.0);
 
-exports.getCurrentTask = function (currentTime) {
-  let list_task = [
+let list_task = [
     1646931600, 1647795600, 1648746000, 1649610000, 1650474000, 1651338000,
     1652202000, 1653066000, 1654016400, 1654880400, 1655744400, 1656608400,
     1657472400, 1658336400, 1659286800, 1660150800, 1661014800, 1661965200,
@@ -9,8 +8,10 @@ exports.getCurrentTask = function (currentTime) {
     1668099600, 1668963600, 1669827600, 1670691600, 1671555600, 1672506000,
     1673370000, 1674234000, 1675184400, 1676048400, 1676912400, 1677603600,
     1678467600, 1679331600, 1680282000, 1681146000, 1682010000, 1682874000,
-    1683738000, 1684602000, 1685552400, 1686416400, 1687280400,
-  ];
+    1683738000, 1684602000, 1685552400, 1686416400, 1687280400, 1688144400
+];
+  
+exports.getCurrentTask = function (currentTime) {
 
   let getCurrentTask = list_task.filter((e) => e > currentTime);
 
@@ -107,7 +108,7 @@ exports.later = function (delay) {
 exports.development = function(user, event){
     console.log(event)
     // only handle the user William Shakehand
-   if(user != "U01C3SA99FW") return;
+   if(user != "U01C3SA99FW") return true;
    
 }
 
@@ -129,13 +130,15 @@ exports.onlyHandleMainThreadEvent = function (thread_ts){
 }
 
 exports.onlyHandlePublicEvent = function (channel_type){
+  if(typeof channel_type === "undefined") return;
      // DO NOT HANDLE IF SEND THROUGH IM
   if (channel_type === "im") return true;
   
 }
 
 exports.onlyHandleIfNotDeletingEvent = function (subtype){
-    if (subtype === "message_deleted" || subtype === "message_changed") return true;
+    if(typeof subtype === "undefined") return false;
+    if (subtype === "message_deleted") return true;
     
 }
 
@@ -144,3 +147,13 @@ exports.onlyHandleIfNotBot = function (user){
     if (user == "U01EVJFP0U8" || user == "U01HEMMPVK2") return true;
     
 }
+
+exports.getRandomInt = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+exports.randomIndex = function (max) {
+  return Math.floor(Math.random() * max);
+};
